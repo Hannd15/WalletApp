10eeb7c2f903ef56c1d1d1b6be66228ec14fc07e
Hannd <LEJHernandez08@gmail.com>
2023-09-06 17:54:35 -0500

    Card activity 06/09/2023

--- app/src/main/java/com/LuisJaimes/walletapp/NewAccountActivity.java
+++ app/src/main/java/com/LuisJaimes/walletapp/NewAccountActivity.java
@@ -2,13 +2,30 @@ package com.LuisJaimes.walletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class NewAccountActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        ivBack = findViewById(R.id.iv_back);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(NewAccountActivity.this, MainActivity.this);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
